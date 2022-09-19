0658cc92f21cbb6fd99041c3780e2329d8b0dc58
Koki Sakano <kosakanoki@gmail.com>
2022-09-19 18:12:02 +0900

    タブを使って機能を切り替え可能にする

--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -1,4 +1,13 @@
-import { Box, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Textarea,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import { useId } from 'react';
 
@@ -8,6 +17,16 @@ const Index: NextPage = () => {
     <Box maxW="1200px" margin="0 auto" p="0 40px">
       <Box aria-labelledby={`characters_header_${id}`} as="section" pt="8">
         <Heading id={`characters_header_${id}`}>Characters</Heading>
+        <Tabs>
+          <TabList>
+            <Tab>文字数カウント</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Textarea placeholder="ここに文字を入力してください" />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
     </Box>
   );
