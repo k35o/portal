567b3a93ec01c42473ec8b2dd4ef15ed0cfd70e5
Koki Sakano <kosakanoki@gmail.com>
2022-09-19 20:28:17 +0900

    状態の作成

---
+++ src/globalStates/characters/index.ts
@@ -0,0 +1,12 @@
+import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { RECOIL_KEY } from '../recoilKeys';
+
+const text = atom<string>({
+  key: RECOIL_KEY.ATOM_CHARACTERS_TEXT,
+  default: '',
+});
+
+export const charactersText = {
+  useTextValue: () => useRecoilValue(text),
+  useTextState: () => useRecoilState(text),
+};
--- src/globalStates/recoilKeys.ts
+++ src/globalStates/recoilKeys.ts
@@ -1,3 +1,4 @@
 export const RECOIL_KEY = {
   ATOM_APP_INFO_LIST: 'atom.appInfoList',
+  ATOM_CHARACTERS_TEXT: 'atom.charactersText',
 } as const;
