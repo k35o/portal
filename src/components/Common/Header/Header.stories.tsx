62a64d0c53acac140ce6ae6f2f8d9891c26d29f5
Koki Sakano <kosakanoki@gmail.com>
2022-09-19 17:59:37 +0900

    このPRには関係ないが、気になるので移動

--- src/components/Common/Header/Header.stories.tsx
+++ src/components/Common/Header/Header.stories.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Header } from './Header';
 
 export default {
-  title: 'Header',
+  title: 'Common/Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
