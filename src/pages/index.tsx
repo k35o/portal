698bf56a2c18bdd6b833468443e3ecb2715dc19e
Koki Sakano <kosakanoki@gmail.com>
2022-09-19 17:04:57 +0900

    pagesにheader追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { AppCardList } from '@/components/Common/AppCard';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { useId } from 'react';
 
 const Home: NextPage = () => {
+  const id = useId();
   return (
     <Box maxW="1200px" margin="0 auto" p="0 40px">
-      <AppCardList />
+      <Box aria-labelledby={`application_header_${id}`} as="section" pt="8">
+        <Heading id={`application_header_${id}`}>Applications</Heading>
+        <AppCardList />
+      </Box>
     </Box>
   );
 };
