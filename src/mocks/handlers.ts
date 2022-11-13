36d0b172ae3b98069fdd24ebe5bad022ec5dbdfa
Koki Sakano <kosakanoki@gmail.com>
2022-11-13 14:32:37 +0900

    errorのstoriesを作る

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { session } from './resolvers/Auth/session';
 import { dummy } from './resolvers/dummy';
 
 export const handlers = [
-  rest.get('/api/auth/session', session),
+  rest.get('/api/auth/session', session(true)),
   rest.post('/api/auth/signIn', dummy),
   rest.post('/api/auth/signOut', dummy),
   rest.get('/api/auth/csrf', dummy),
