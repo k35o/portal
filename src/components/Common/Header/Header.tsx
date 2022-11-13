b5835fc6e011b12f04ec4eb12ba579bc1a1be536
Koki Sakano <kosakanoki@gmail.com>
2022-11-13 12:44:06 +0900

    loginとoutを可能に

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
   Box,
+  Button,
   Flex,
   Heading,
   IconButton,
@@ -8,9 +9,12 @@ import {
   useColorMode,
 } from '@chakra-ui/react';
 import { Link } from '@/components/Common/Link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { status } = useSession();
+
   return (
     <Box as="header" borderBottom="1px solid" borderBottomColor="gray.500">
       <Flex maxW="1200px" h="16" margin="0 auto" p="0 40px" align="center">
@@ -28,6 +32,14 @@ export const Header = () => {
             icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
             onClick={toggleColorMode}
           />
+        </Box>
+        <Box>
+          {status === 'unauthenticated' && (
+            <Button onClick={() => signIn('google')}>Sign in</Button>
+          )}
+          {status === 'authenticated' && (
+            <Button onClick={() => signOut()}>Log out</Button>
+          )}
         </Box>
       </Flex>
     </Box>
