111896d3b5ef7ed68554aebf0b028a3b26c6306d
Koki Sakano <kosakanoki@gmail.com>
2022-11-13 17:35:34 +0900

    appに導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app';
 import { Layout } from '@/components/Common/Layout';
 import { Session } from 'next-auth';
 import { AppProvider } from '@/provider/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
-    <AppProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AppProvider>
+    <>
+      <AppProvider session={pageProps.session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppProvider>
+      <Analytics />
+    </>
   );
 }
 
